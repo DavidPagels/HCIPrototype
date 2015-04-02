d6ed522a015871c4542d377410e1f6315deb699d
DavidPagels <pagel093@morris.umn.edu>
2015-04-01 21:08:39 -0500

    All activities added, working on populating activities

---
+++ app/src/main/java/com/example/david/hciprototype/GPSHandler.java
@@ -0,0 +1,7 @@
+package com.example.david.hciprototype;
+
+/**
+ * Created by David on 4/1/2015.
+ */
+public class GPSHandler {
+}
--- app/src/main/java/com/example/david/hciprototype/SpeedPrompt.java
+++ app/src/main/java/com/example/david/hciprototype/SpeedPrompt.java
@@ -1,35 +1,24 @@
 package com.example.david.hciprototype;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class SpeedPrompt extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button submitButton = (Button) findViewById(R.id.newLoc);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, SetLocation.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
+        setContentView(R.layout.activity_speed_prompt);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_speed_prompt, menu);
         return true;
     }
 
