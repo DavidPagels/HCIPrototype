001a573bf158cfe426ddad3e7faca4fb92b7d3a4
harre096 <harre096@umn.edu>
2015-04-02 01:21:51 -0500

    added Labels and spinner to newActivity

--- app/src/main/java/com/example/david/hciprototype/NewActivity.java
+++ app/src/main/java/com/example/david/hciprototype/NewActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class NewActivity extends ActionBarActivity {
@@ -12,6 +14,15 @@ public class NewActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
+
+        Spinner spinner = (Spinner) findViewById(R.id.locMenu);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.locations_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
 
